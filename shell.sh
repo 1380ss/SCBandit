d839ad7bd91c63b1e2c09f53f862568e3c6d2575
1380ss <to.li@mail.utoronto.ca>
2023-03-31 12:08:34 -0400

    fde

--- shell.sh
+++ shell.sh
@@ -1,2 +1,16 @@
-git config --global user.name '1380ss'
-git config --global user.email 'to.li@mail.utoronto.ca'
+git config --local -l
+git config --local user.name '1380ss'
+git config --local user.email 'to.li@mail.utoronto.ca'
+git config --global -l
+git config --list --local | grep user
+
+git fetch origin master
+git remote -v
+
+git branch -r 
+
+ls ./.git/refs/heads/ 
+git fetch --all
+
+# do following in future:
+git fetch --all
